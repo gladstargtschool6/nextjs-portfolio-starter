bb9c56e90bbcb1fc343ecc3bad10927a943cc104
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-03 18:03:33 +0100

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Your Name.
-      <a href="/feed.xml">RSS</a>
+      <a href="/annotations.xml">RSS</a>
       <style jsx>{`
         a {
           float: right;
